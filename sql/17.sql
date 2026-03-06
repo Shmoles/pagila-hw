955f7575bebbbbf132d76dd949cf8824bfb8b118
shmoles <shmoles@users.noreply.github.com>
2026-03-06 15:13:06 -0800

    Fix sql/17 to join customer via rental

--- sql/17.sql
+++ sql/17.sql
@@ -1,9 +1,11 @@
-SELECT country.country, sum(payment.amount) AS profit
+SELECT
+  country.country,
+  SUM(payment.amount) AS profit
 FROM payment
-JOIN rental ON payment.rental_id = rental.rental_id
-JOIN customer ON payment.customer_id = customer.customer_id
-JOIN address ON customer.address_id = address.address_id
-JOIN city ON address.city_id = city.city_id
-JOIN country ON city.country_id = country.country_id
+JOIN rental   ON payment.rental_id = rental.rental_id
+JOIN customer ON rental.customer_id = customer.customer_id
+JOIN address  ON customer.address_id = address.address_id
+JOIN city     ON address.city_id = city.city_id
+JOIN country  ON city.country_id = country.country_id
 GROUP BY country.country
 ORDER BY country.country;
